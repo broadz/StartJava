231a32e17ba71240c500b4f966c32b07f0f40ba7
broadz <topima27@gmail.com>
2020-11-30 12:56:24 +0100

    1

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+	public static void main(String[] args) {
+
+		byte core = 4;
+		short ozu = 8;
+		int systype = 64;
+		long procprice = 297;
+		float minghz = 1.6f;
+		double maxghz = 3.40d;  
+		char firstletterbrand = 'X';
+		boolean mx150 = true;
+
+			System.out.println(core);
+			System.out.println(ozu);
+			System.out.println(systype);
+			System.out.println(procprice);
+			System.out.println(minghz);
+			System.out.println(maxghz);
+			System.out.println(firstletterbrand);
+			System.out.println(mx150);
+	}
+}
