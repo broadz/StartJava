38322f5ae1a9c67005929e602763251695a2dc6e
broadz <topima27@gmail.com>
2020-11-30 21:46:15 +0100

    1

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,22 +1,21 @@
 public class Variable {
-	public static void main(String[] args) {
-
+	public static void main(String[] args){
 		byte core = 4;
-		short ozu = 8;
+		short ram = 8;
 		int systype = 64;
 		long procprice = 297;
 		float minghz = 1.6f;
 		double maxghz = 3.40d;  
-		char firstletterbrand = 'X';
+		char firstLetterBrand = 'X';
 		boolean mx150 = true;
 
-			System.out.println(core);
-			System.out.println(ozu);
-			System.out.println(systype);
-			System.out.println(procprice);
-			System.out.println(minghz);
-			System.out.println(maxghz);
-			System.out.println(firstletterbrand);
-			System.out.println(mx150);
+		System.out.println(core);
+		System.out.println(ram);
+		System.out.println(systype);
+		System.out.println(procprice);
+		System.out.println(minghz);
+		System.out.println(maxghz);
+		System.out.println(firstLetterBrand);
+		System.out.println(mx150);
 	}
 }
